322471ce376c08bc3d0755da3042ee8bafa3b6f6
Paul Thrasher <thrashr888@gmail.com>
2019-04-22 15:17:55 -0700

    start playing with monkey
    Signed-off-by: Paul Thrasher <thrashr888@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { NewEnvironment, Lexer, Parser, Eval } from 'monkey-typescript';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.env = NewEnvironment();
+
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      debug: '',
+      lines: `let double = fn(n){ n * 2 }
+let a = double(102)
+a * 21 - 84
+let hello = fn(name){ "Hello, " + name + "!" }
+hello("Paul")
+let bigNum = fn(x){x>500}
+`.split('\n'),
+    };
+  }
+
+  eval(line) {
+    let l = new Lexer(line);
+    let p = new Parser(l);
+    let program = p.ParseProgram();
+
+    if (p.Errors().length !== 0) {
+      return [p.Errors(), null];
+    } else {
+      return [null, Eval(program, this.env)];
+    }
+  }
+
+  onChange(e) {
+    let input = e.target.value;
+    let lines = input.split('\n');
+
+    this.setState({
+      debug: input,
+      lines,
+    });
+  }
+
+  renderOutput(l) {
+    let out = this.eval(l);
+    if (!out[1]) {
+      return '';
+    }
+    return out[1].Inspect().replace(/\n/g, ' ');
+  }
+
   render() {
+    let out = this.state.lines
+      .map(l => {
+        return this.renderOutput(l);
+      })
+      .join('\n');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <textarea
+          cols="45"
+          rows="35"
+          onKeyUp={this.onChange}
+          defaultValue={this.state.lines.join('\n')}
+          focus="true"
+        />
+        <textarea cols="25" rows="35" value={out} readOnly />
+        <br />
+        <pre>
+          <code>{this.state.debug}</code>
+        </pre>
       </div>
     );
   }
